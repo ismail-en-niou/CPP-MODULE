4af97c52f97ffb3837df8d6993da16a62d61a9b7
Ismail  En-niou <ismail-en-niou>
2025-08-02 11:33:57 +0100

    Update Makefile

---
+++ cpp_module_01/ex06/main.cpp
@@ -0,0 +1,41 @@
+#include "Harl.hpp"
+#include <iostream>
+
+
+int main(int ac, char const *av[])
+{
+    Harl harl;
+    if (ac != 2)
+        return 1;
+    std::string  levels[4] = {"DEBUG" , "INFO" , "WARNING" , "ERROR"};
+    int i = 0;
+    while (i < 4)
+    {
+        if (levels[i] == av[1])
+            break;
+        i++;
+    }
+    switch (i)
+    {
+        case 0:
+            std::cout << "[ DEBUG ]" << std::endl;
+            harl.complain("DEBUG");
+            break;
+        case 1:
+            std::cout << "[ INFO ]" << std::endl;
+            harl.complain("INFO");
+            break;
+        case 2:
+            std::cout << "[ WARNING ]" << std::endl;
+            harl.complain("WARNING");
+            break;
+        case 3:
+            std::cout << "[ ERROR ]" << std::endl;
+            harl.complain("ERROR");
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" <<std::endl;
+            break;
+    }
+    return 0;
+}
